f8c8271dac34669367e8448695754493b30e1e4c
Connor <clarksoc@sheridancollege.ca>
2020-07-21 16:48:02 -0400

    Fixed back press on ChatOverviewScreen

--- lib/screens/chat_overview_screen.dart
+++ lib/screens/chat_overview_screen.dart
@@ -4,6 +4,7 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:tagnessappchat/screens/main_screen.dart';
 
 import 'login_screen.dart';
 import 'settings_screen.dart';
@@ -72,62 +73,75 @@ class _ChatOverviewScreenState extends State<ChatOverviewScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).accentColor,
-        title: Text(
-          "Welcome",
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        centerTitle: true,
-        actions: <Widget>[
-          PopupMenuButton<Selection>(
-            onSelected: onMenuPress,
-            itemBuilder: (BuildContext context) {
-              return selections.map((Selection selection) {
-                return PopupMenuItem<Selection>(
-                  value: selection,
-                  child: Row(
-                    children: <Widget>[
-                      Icon(selection.iconData),
-                      Container(
-                        width: 10.0,
-                      ),
-                      Text(selection.title),
-                    ],
-                  ),
-                );
-              }).toList();
-            },
-          )
-        ],
-      ),
-      body: Stack(
-          children: <Widget>[
-            Container(
-              child: StreamBuilder(
-                stream: fireInstance.collection("users").snapshots(),
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    return Center(
-                      child: CircularProgressIndicator(
-                        valueColor: AlwaysStoppedAnimation<Color>(Colors.red),
-                      ),
-                    );
-                  } else {
-                    return ListView.builder(
-                        itemBuilder: (context, index) => buildItem(context,//calls the build_item widget and passes in the current User
-                            snapshot.data.documents[index], currentUserId),
-                        itemCount: snapshot.data.documents.length,
-                      );
-                  }
-                },
-              ),
-            ),
-            Positioned(child: isLoading? const Loading() : Container())//Displays loading circle if loading users in the database or an empty container if there are no more
+    return WillPopScope(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).accentColor,
+          title: Text(
+            "Welcome",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true,
+          actions: <Widget>[
+            PopupMenuButton<Selection>(
+              onSelected: onMenuPress,
+              itemBuilder: (BuildContext context) {
+                return selections.map((Selection selection) {
+                  return PopupMenuItem<Selection>(
+                    value: selection,
+                    child: Row(
+                      children: <Widget>[
+                        Icon(selection.iconData),
+                        Container(
+                          width: 10.0,
+                        ),
+                        Text(selection.title),
+                      ],
+                    ),
+                  );
+                }).toList();
+              },
+            )
           ],
         ),
+        body: Stack(
+            children: <Widget>[
+              Container(
+                child: StreamBuilder(
+                  stream: fireInstance.collection("users").snapshots(),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return Center(
+                        child: CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation<Color>(Colors.red),
+                        ),
+                      );
+                    } else {
+                      return ListView.builder(
+                          itemBuilder: (context, index) => buildItem(context,//calls the build_item widget and passes in the current User
+                              snapshot.data.documents[index], currentUserId),
+                          itemCount: snapshot.data.documents.length,
+                        );
+                    }
+                  },
+                ),
+              ),
+              Positioned(child: isLoading? const Loading() : Container())//Displays loading circle if loading users in the database or an empty container if there are no more
+            ],
+          ),
+      ),
+      onWillPop: onBackPress,
     );
+  }
+  Future<bool> onBackPress() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => MainScreen(
+        ),
+      ),
+    );
+    return Future.value(false);
   }
 }
 
